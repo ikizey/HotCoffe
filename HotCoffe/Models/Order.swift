0becd8c4d338d220e7e337aa0bb431d21837edff
ikizey <ikizey@icloud.com>
2021-08-01 02:14:08 +0300

    add order init

--- HotCoffe/Models/Order.swift
+++ HotCoffe/Models/Order.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 enum CoffeType: String, Codable, CaseIterable {
+    
     case cappuccino
     case latte
     case java
@@ -18,14 +19,35 @@ enum CoffeType: String, Codable, CaseIterable {
 }
 
 enum CoffeSize: String, Codable, CaseIterable {
+    
     case small
     case medium
     case large
 }
 
 struct Order: Codable {
+    
     let name: String
     let email: String
     let type: CoffeType
     let size: CoffeSize
+}
+
+extension Order {
+    
+    init?(_ viewModel: AddCofferOrderViewModel) {
+        guard let name = viewModel.name,
+              let email = viewModel.email,
+              let selectedType = CoffeType(
+                rawValue: viewModel.selectedType!.lowercased()),
+              let seletedSize = CoffeSize(
+                rawValue: viewModel.selectedSize!.lowercased()) else {
+            return nil
+        }
+        
+        self.name = name
+        self.email = email
+        self.type = selectedType
+        self.size = seletedSize
+    }
 }
