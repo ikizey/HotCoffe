fd043e0f31a7483d07c50a417433005299280d26
ikizey <ikizey@icloud.com>
2021-07-31 23:40:16 +0300

    initial interface setup

---
+++ HotCoffe/Controllers/AddOrderViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddOrderViewController.swift
+//  HotCoffe
+//
+//  Created by PrincePhoenix on 31.07.2021.
+//
+
+import UIKit
+
+class AddOrderViewController: UIViewController {
+    
+}
---
+++ HotCoffe/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrdersTableViewController.swift
+//  HotCoffe
+//
+//  Created by PrincePhoenix on 31.07.2021.
+//
+
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+    
+}
+
