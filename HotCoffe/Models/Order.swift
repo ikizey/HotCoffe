43dc3af2d2568feb768620b6611a8169780555eb
ikizey <ikizey@icloud.com>
2021-08-01 00:19:08 +0300

    add basic model

---
+++ HotCoffe/Models/Order.swift
@@ -0,0 +1,31 @@
+//
+//  Order.swift
+//  HotCoffe
+//
+//  Created by PrincePhoenix on 01.08.2021.
+//
+
+import Foundation
+
+enum CoffeType: String, Codable {
+    case cappuccino
+    case latte
+    case java
+    case espresso
+    case mocha
+    case americano
+    case cortado
+}
+
+enum CoffeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    let name: String
+    let email: String
+    let type: CoffeType
+    let size: CoffeSize
+}
