17dc2e80f4fe00e2befe5d5ca3afe53db7e93326
ikizey <ikizey@icloud.com>
2021-08-01 00:53:19 +0300

    display models in TableView

--- HotCoffe/Controllers/OrdersTableViewController.swift
+++ HotCoffe/Controllers/OrdersTableViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    var orderListViewModel = OrderListViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,14 +24,28 @@ class OrdersTableViewController: UITableViewController {
         
         let resource = Resource<[Order]>(url: coffeOrdersURL)
         
-        Webservice().load(resource: resource) { result in
+        Webservice().load(resource: resource) { [weak self] result in
             switch result {
             case .success(let orders):
-                print(orders)
+                self?.orderListViewModel.ordersViewModel = orders.map(OrderViewModel.init)
+                self?.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        orderListViewModel.ordersViewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let viewModel = orderListViewModel.orderViewModel(at: indexPath.row)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderTableViewCell", for: indexPath)
+        cell.textLabel?.text = viewModel.type
+        cell.detailTextLabel?.text = viewModel.size
+        return cell
     }
 }
 
